7a8aa6efe558676217c4b894451a0d620c09cc92
ly2011 <1518550424@qq.com>
2019-02-14 23:00:22 +0800

    添加html-webpack-plugin插件，还未能接入该插件

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,22 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 const nodePathParts = ['./src', './node_modules'].map(p => path.resolve(p))
 
 module.exports = {
   // https://github.com/webpack/changelog-v5/blob/master/README.md#persistent-caching
   cache: {
-    type: 'filesystem',
+    type: 'filesystem'
   },
   entry: './src/index.js',
   output: {
     path: path.join(__dirname, '/dist'),
-    filename: 'bundle.js',
+    // filename: 'bundle.js'
+    filename: 'bundle-[contenthash].js'
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.jsx'],
-    modules: nodePathParts,
+    modules: nodePathParts
   },
   module: {
     rules: [
@@ -22,9 +24,10 @@ module.exports = {
         test: /\.jsx?$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
-        },
-      },
-    ],
+          loader: 'babel-loader'
+        }
+      }
+    ]
   },
+  plugins: [new HtmlWebpackPlugin()]
 }
