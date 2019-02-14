253f088147f5eef3cb3c3df9e678b08dc7f6a302
ly2011 <1518550424@qq.com>
2019-02-14 22:25:38 +0800

    <初始化项目>

---
+++ babel.config.js
@@ -0,0 +1,21 @@
+module.exports = function (api) {
+  const env = api.env()
+
+  const config = {
+    presets: [
+      '@babel/preset-react',
+      [
+        '@babel/env',
+        {
+          modules: env === 'test' ? 'commonjs' : false,
+          targets: {
+            browsers: ['last 2 versions'],
+          },
+        },
+      ],
+    ],
+    plugins: ['@babel/proposal-class-properties', '@babel/proposal-object-rest-spread'],
+  }
+
+  return config
+}
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => (
+  <div className="app">
+    <header className="header">
+      <h2 className="title">App Page</h2>
+    </header>
+  </div>
+)
+
+ReactDOM.render(<App />, document.getElementById('root'))
